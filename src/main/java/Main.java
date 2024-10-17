51b4dfbcbc9c6e49c20fa05ecde17391b2da6986
Victor Salimgareev <Shiverz286@gmail.com>
2024-10-17 18:08:11 +0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static final String REMOTE_URI =
+            "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+        //Объект запроса
+        HttpGet request = new HttpGet(REMOTE_URI);
+        //Удаленный сервис
+        CloseableHttpResponse response = httpClient.execute(request);
+        //Преобразование json
+        List<Cat> cats = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
+
+        cats.stream()
+                .filter(cat -> cat.getUpvotes() !=null)
+                .forEach(System.out::println);
+    }
+}
